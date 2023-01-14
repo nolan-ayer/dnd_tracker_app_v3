3758d488541ebc75f4f34da092be37af36d6ce77
nowolan <115511250+nowolan@users.noreply.github.com>
2023-01-13 22:26:44 -0800

    box shadow UI elements; minor revision to welcome page

--- src/components/UI/Welcome/Welcome.js
+++ src/components/UI/Welcome/Welcome.js
@@ -1,48 +1,56 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import AuthContext from "../../../context/auth-context";
-import styles from "./Welcome.module.css";
+// import styles from "./Welcome.module.css";
 
 const Welcome = () => {
   const authCtx = useContext(AuthContext);
   return (
     <div className="contentMain">
-      <h3 className="centered">Welcome!</h3>
-      <section className="contentMainPlacementHelper">
-        <section className="mainContentsStandalone">
-          This application is designed using the Divinity 2 Original Sin
-          leveling architecture and is intended for use by both players and
-          their Dungeon Masters. Within the app, you will find organizational
-          tools for keeping track of charactersheets and assorted notes for your
-          game.
-        </section>
-        {!authCtx.isLoggedIn ? (
+      <div className="contentMain">
+        <h3 className="centered">Welcome!</h3>
+        <section className="contentMainPlacementHelper">
           <section className="mainContentsStandalone">
-            To begin, please login with your username and password. If you
-            haven't created an account yet, just click {""}
-            <Link className="embeddedLink" to="/signup">
-              here
-            </Link>
-            {""} to get started!
+            This application is designed using the Divinity 2 Original Sin
+            leveling architecture and is intended for use by both players and
+            their Dungeon Masters. Within the app, you will find organizational
+            tools for keeping track of charactersheets and assorted notes for
+            your game.
           </section>
-        ) : (
-          <>
-            <section className="contentMain">Fixes Coming Soon:</section>
-            <section className="contentMain">
-              onCardEdit, values can exceed the maxValue set in the initial form
+          {!authCtx.isLoggedIn && (
+            <section className="mainContentsStandalone">
+              To begin, please login with your username and password. If you
+              haven't created an account yet, just click {""}
+              <Link className="embeddedLink" to="/signup">
+                here
+              </Link>
+              {""} to get started!
             </section>
-            <section className="contentMain">
-              Custom hooks should be used for HTTP requests
-            </section>
-            <section className="contentMain">
-              {`   Scaling issue at width ~450px; potential Chrome DevTools
-              responsive dimensions bug; does not happen when window is resized
-              & responsive is disabled; chrome revision: e7c5703604daa9cc128ccf5a5d3e993513758913-refs/branch-heads/5414@{#1172}`}
-            </section>
-            <section className="contentMain">Add inventory to cards</section>
-          </>
-        )}
-      </section>
+          )}
+        </section>
+      </div>
+      {authCtx.isLoggedIn && (
+        <div className="contentMainPlacementHelper">
+          <section className="mainContentsStandalone">
+            Fixes Coming Soon:
+          </section>
+          <section className="mainContentsStandalone">
+            onCardEdit, values can exceed the maxValue as defined in the initial
+            form
+          </section>
+          <section className="mainContentsStandalone">
+            Custom hooks should be used for HTTP requests
+          </section>
+          <section className="mainContentsStandalone">
+            Scaling issue at width ~450px; potential Chrome DevTools responsive
+            dimensions bug; does not happen when window is resized & responsive
+            is disabled
+          </section>
+          <section className="mainContentsStandalone">
+            Add inventory to cards
+          </section>
+        </div>
+      )}
     </div>
   );
 };
