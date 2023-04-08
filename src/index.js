eb9f1ec83af0301f8e7560223a65fa0922bda6d9
nowolan <115511250+nowolan@users.noreply.github.com>
2023-04-07 23:54:37 -0700

    hashrouter to support github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/auth-context";
 import App from "./App";
 import "./index.css";
@@ -8,8 +8,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthContextProvider>
 );
